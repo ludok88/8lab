1276d41509a08f879cca57779c0239c1338d26be
ludok88 <108759894+ludok88@users.noreply.github.com>
2023-01-02 21:08:45 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
