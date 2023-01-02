b3fe2a1cb63b162d34a7ba3206a3e21af70db244
ludok88 <108759894+ludok88@users.noreply.github.com>
2023-01-02 21:08:14 +0300

    Create ToDoItem.js

---
+++ ToDoItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function ToDoItem() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <label htmlFor="chk">Text for checkbox</label>
+        </div>
+    );
+}
+
+export default ToDoItem;
