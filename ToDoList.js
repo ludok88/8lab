9dd627c41b8a6e8c58f421721846a07f4330056e
ludok88 <108759894+ludok88@users.noreply.github.com>
2023-01-02 21:08:29 +0300

    Create ToDoList.js

---
+++ ToDoList.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ToDoItem from "./ToDoItem";
+
+function ToDoList() {
+    return (
+        <div>
+            <ToDoItem />
+            <ToDoItem />
+            <ToDoItem />
+            <ToDoItem />
+        </div>
+    );
+}
+
+export default ToDoList;
