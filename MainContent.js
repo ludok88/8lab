eea899e8c6f572615b3bca2a0625bb2a4b3b2954
ludok88 <108759894+ludok88@users.noreply.github.com>
2023-01-02 21:05:39 +0300

    Create MainContent.js

---
+++ MainContent.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ToDoList from "./ToDoList";
+
+function MainContent() {
+    return (
+        <main>
+            <p>This is the main element</p>
+            <ToDoList/>
+        </main>
+    );
+}
+
+export default MainContent;
